6596f283c22edf1781bc13d4823159734eed7eaa
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-04-03 21:03:16 -0700

    edit twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,4 +1,16 @@
-var twoSum = function(nums, target) {
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+const twoSum = function(nums, target) {
     let possibleMatches = {};
 
     for(let i=0; i < nums.length; i++) {
@@ -11,5 +23,3 @@ var twoSum = function(nums, target) {
     }
 
 };
-
-twoSum([3, 3], 6);
