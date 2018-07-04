9201b8a6f9aa7723c3b189200ac5afe6c6639abb
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-07-03 17:24:21 -0700

    add solution with better time complexity

--- Ruby/best_time_to_buy_and_sell_stock.rb
+++ Ruby/best_time_to_buy_and_sell_stock.rb
@@ -16,14 +16,43 @@
 # Output: 0
 # Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
+# def max_profit(prices)
+#   return 0 if prices.length <= 1
+#
+#   max_profit = 0
+#   prices.each_with_index do |buy, idx|
+#     (prices[idx + 1..-1]).each do |sell|
+#       profit = sell - buy
+#       if profit > max_profit
+#         max_profit = profit
+#       end
+#     end
+#   end
+#   max_profit
+# end
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+
 def max_profit(prices)
   return 0 if prices.length <= 1
+  min_price = prices[0]
+  max_profit = 0
 
-  profits = []
-  prices.each_with_index do |buy, idx|
-    (prices[idx + 1..-1]).each do |sell|
-      profits << sell - buy
+  prices[1..-1].each do |price|
+    if price < min_price
+      min_price = price
+    else
+      profit = price - min_price
+
+      if profit > max_profit
+        max_profit = profit
+      end
     end
   end
-  profits.max <= 0 ? 0 : profits.max
+
+  max_profit
 end
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
