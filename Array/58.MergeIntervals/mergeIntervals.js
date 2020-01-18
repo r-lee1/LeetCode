32015f17ca25ba5068c739d4e666deda8dca6b3f
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2020-01-17 16:03:31 -0800

    edit MergeIntervals

--- Array/58.MergeIntervals/mergeIntervals.js
+++ Array/58.MergeIntervals/mergeIntervals.js
@@ -11,3 +11,47 @@
 // Output: [[1,5]]
 // Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 // NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+
+function merge(intervals) {
+
+  /*
+  - Sort Intervals
+  - Go through the list of Intervals:
+    If the end time of the current interval is >==
+    to the start time of the next, then the intervals need to be merged
+  */
+  if (intervals.length === 0) {
+    return intervals;
+  }
+
+  intervals.sort((a, b) => {
+    return a[0] - b[0];
+  });
+
+  const mergedIntervals = [];
+  let startTime = intervals[0][0];
+  let endTime = intervals[0][1];
+
+
+  for (let i = 0; i < intervals.length; i++) {
+    let nextStartTime = undefined;
+    let nextEndTime = undefined;
+
+    if (i !== intervals.length - 1) {
+      nextStartTime = intervals[i+1][0];
+      nextEndTime = intervals[i+1][1];
+    }
+
+//Check that current interval is not the last
+//and end time greater than or equal to start time of next interval
+    if (nextStartTime !== undefined && endTime >= nextStartTime) {
+      endTime = Math.max(endTime, nextEndTime);
+    } else {
+      mergedIntervals.push([startTime, endTime]);
+      startTime = nextStartTime;
+      endTime = Math.max(endTime, nextEndTime);
+    }
+  }
+
+  return mergedIntervals;
+}
