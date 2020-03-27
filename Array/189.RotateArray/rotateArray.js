a3390e9ad7ba3920f8ceb52636edaf20e873f887
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2020-03-26 21:21:34 -0700

    add RotateArray

---
+++ Array/189.RotateArray/rotateArray.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+var rotate = function(nums, k) {
+
+    if (nums.length < 2) {
+        return nums;
+    }
+
+    const rotatedArray = [];
+
+    for(let i = 0; i < nums.length; i++) {
+      let rotateToIndex = i + k;
+
+      if (rotateToIndex >= nums.length) {
+          rotateToIndex = rotateToIndex - nums.length;
+      }
+
+      rotatedArray[rotateToIndex] = nums[i];
+    }
+
+    return rotatedArray;
+};
