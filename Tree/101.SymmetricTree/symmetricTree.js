f7c1a8c5cb3d0110dab9788116aeaf54cfd2e978
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2021-03-31 23:10:52 -0700

    add recursive solution to SymmetricTree

--- Tree/101.SymmetricTree/symmetricTree.js
+++ Tree/101.SymmetricTree/symmetricTree.js
@@ -13,45 +13,89 @@
 /*
 
 */
-var isSymmetric = function(root) {
 
-    if (!root) {
-        return true;
-    }
+//Recursive
 
-    return isMirror(root.left, root.right);
+// const isSymmetric = (root) => {
+//     if (root === null) return true;
+//     return helper(root.left, root.right);
+// };
+//
+// const helper = (n1, n2) => {
+//     if (n1 === null && n2 === null) return true;
+//     if (n1 === null || n2 === null) return false;
+//     return (n1.val === n2.val) && helper(n1.left, n2.right) && helper(n1.right, n2.left);
+// };
 
 
-};
+// var isSymmetric = function(root) {
+//     const leftSubtree = helperL(root.left);
+//     const rightSubtree = helperR(root.right);
 
-function isMirror(leftSubtreeRoot, rightSubtreeRoot) {
+//     if (leftSubtree.length !== rightSubtree.length) return false;
 
-    //Have separate queues for each subtree
-    let leftSubtreeQueue = [leftSubtreeRoot], rightSubtreeQueue = [rightSubtreeRoot];
+//     for (let i = 0; i < leftSubtree.length; i++) {
+//         if (leftSubtree[i] !== rightSubtree[i]) return false;
+//     }
 
-    //BST
-    while (leftSubtreeQueue.length > 0 || rightSubtreeQueue.length > 0) {
+//     return true;
+// };
 
-        let leftSubtreeNode = leftSubtreeQueue.shift();
-        let rightSubtreeNode =  rightSubtreeQueue.shift();
+// const helperL = (node) => {
+//     if (node === null) return [null];
+//     let left = helperL(node.left);
+//     let right = helperL(node.right);
 
-        if (!leftSubtreeNode && !rightSubtreeNode) {
-            continue;
-        }
+//     return [...left, ...right, node.val];
+// }
 
-        if (!leftSubtreeNode || !rightSubtreeNode || leftSubtreeNode.val !== rightSubtreeNode.val) {
-            return false;
-        }
+// const helperR = (node) => {
+//     if (node === null) return [null];
+//     let right = helperR(node.right);
+//     let left = helperR(node.left);
 
-        //Scan left subtree from left to right
-        //and right subtree from right to left
+//     return [...right, ...left, node.val];
+// }
 
-        leftSubtreeQueue.push(leftSubtreeNode.left);
-        leftSubtreeQueue.push(leftSubtreeNode.right);
-        rightSubtreeQueue.push(rightSubtreeNode.right);
-        rightSubtreeQueue.push(rightSubtreeNode.left);
-
-    }
-
-    return true;
-}
+// var isSymmetric = function(root) {
+//
+//     if (!root) {
+//         return true;
+//     }
+//
+//     return isMirror(root.left, root.right);
+//
+//
+// };
+//
+// function isMirror(leftSubtreeRoot, rightSubtreeRoot) {
+//
+//     //Have separate queues for each subtree
+//     let leftSubtreeQueue = [leftSubtreeRoot], rightSubtreeQueue = [rightSubtreeRoot];
+//
+//     //BST
+//     while (leftSubtreeQueue.length > 0 || rightSubtreeQueue.length > 0) {
+//
+//         let leftSubtreeNode = leftSubtreeQueue.shift();
+//         let rightSubtreeNode =  rightSubtreeQueue.shift();
+//
+//         if (!leftSubtreeNode && !rightSubtreeNode) {
+//             continue;
+//         }
+//
+//         if (!leftSubtreeNode || !rightSubtreeNode || leftSubtreeNode.val !== rightSubtreeNode.val) {
+//             return false;
+//         }
+//
+//         //Scan left subtree from left to right
+//         //and right subtree from right to left
+//
+//         leftSubtreeQueue.push(leftSubtreeNode.left);
+//         leftSubtreeQueue.push(leftSubtreeNode.right);
+//         rightSubtreeQueue.push(rightSubtreeNode.right);
+//         rightSubtreeQueue.push(rightSubtreeNode.left);
+//
+//     }
+//
+//     return true;
+// }
