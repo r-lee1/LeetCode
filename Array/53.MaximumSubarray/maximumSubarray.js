d60f0d4d9d17c340089b1e68d008a242885bf94e
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2021-04-13 20:24:14 -0700

    add optimized solution to MaximumSubarray

--- Array/53.MaximumSubarray/maximumSubarray.js
+++ Array/53.MaximumSubarray/maximumSubarray.js
@@ -1,18 +1,43 @@
-var maxSubArray = function(nums) {
-    let maxSum = -Infinity;
+const maxSubArray = (nums) => {
+   let currentSubarraySum = nums[0];
+   let maxSubarraySum = nums[0];
 
-   for (let i = 0; i < nums.length; i++) {
-       let sum = 0;
-       for (let j = i; j < nums.length; j++) {
-           sum += nums[j];
-           maxSum = Math.max(sum, maxSum);
-       }
+   for (let i = 1; i < nums.length; i++) {
+       currentSubarraySum += nums[i];
+       if (currentSubarraySum < nums[i]) currentSubarraySum = nums[i];
+       maxSubarraySum = Math.max(maxSubarraySum, currentSubarraySum);
    }
 
-    return maxSum;
+   return maxSubarraySum;
 };
 
 /*
-Time: O(N^2)
+1. Initialize a currentSubarraySum and a maxSubarraySum to the first element
+2. Loop through nums, starting from element at index 1
+   - Add nums[i] to currentSubarraySum
+   - If new currentSubarraySum is less than nums[i], then discard the previous elements and start currentSubarraySum over at nums[i]
+   - If currentSubarraySum is greater than maxSubarraySum, then update it
+3. Return maxSubarraySum
+
+Time: O(N)
 Space: O(1)
 */
+
+// var maxSubArray = function(nums) {
+//     let maxSum = -Infinity;
+//
+//    for (let i = 0; i < nums.length; i++) {
+//        let sum = 0;
+//        for (let j = i; j < nums.length; j++) {
+//            sum += nums[j];
+//            maxSum = Math.max(sum, maxSum);
+//        }
+//    }
+//
+//     return maxSum;
+// };
+//
+// /*
+// Time: O(N^2)
+// Space: O(1)
+// */
