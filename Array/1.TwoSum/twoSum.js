2632c6e8ce210125f917710217e5f5fd7acb0235
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2021-08-29 20:35:23 -0700

    Edit TwoSum

--- Array/1.TwoSum/twoSum.js
+++ Array/1.TwoSum/twoSum.js
@@ -4,23 +4,28 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let h = {};
-
-    for (let i = 0; i < nums.length; i++) {
-        let num = nums[i];
-        let match = target - num;
-        if (h[match] >= 0 && h[match] !== i) return [i, h[match]];
-        h[num] = i;
-    }
+  let numMap = {}; //{2:0,}
+  for (let i = 0; i < nums.length; i++) {
+      let match = target - nums[i];
+      if (numMap[match] !== undefined) return [i, numMap[match]];
+      else numMap[nums[i]] = i;
+  }
 };
+// Time: O(N), Space: O(N)
 
 /*
-Time: O(N)
-Space: O(N)
-
 - Create a hash table to store the element in nums as key and the index as value
 - Iterate through nums and for nums[i], check if its complement (target - nums[i]) exists in the hash table
     - If it does, return [i, h[complement]]
     - Insert nums[i] into the hash table
 
 */
+
+// var twoSum = function(nums, target) {
+//   for (let i = 0; i < nums.length - 1; i++) {
+//       for (let j = i+1; j < nums.length; j++) {
+//           if (nums[i] + nums[j] === target) return [i, j];
+//       }
+//   }
+// };
+// Time: O(N^2), Space: O(1)
