0bde4231ac19151186c245d983521fdce8b5cec3
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2021-09-05 20:54:09 -0700

    Improve MeetingRoomsII

--- Array/253.MeetingRoomsII/meetingRoomsII.js
+++ Array/253.MeetingRoomsII/meetingRoomsII.js
@@ -1,20 +1,51 @@
-var minMeetingRooms = function(intervals) {
-    if (intervals.length === 1) return 1;
+const minMeetingRooms = (intervals) => {
+    const start = [];
+    const end = [];
 
-    const latestTime = intervals.reduce((max, interval) => {
-        return Math.max(max, interval[1]);
-    }, 0);
-
-    const meetingTracker = new Array(latestTime+1).fill(0);
-
-    for (let i = 0; i < intervals.length; i++) {
-        const interval = intervals[i];
-        for (let j = interval[0]; j < interval[1]; j++) {
-            meetingTracker[j]++;
-        }
+    for(let i = 0; i < intervals.length; i++) {
+        start.push(intervals[i][0]);
+        end.push(intervals[i][1]);
     }
 
-    return Math.max(...meetingTracker);
+    start.sort((a,b) => a-b);
+    end.sort((a,b) => a-b);
+
+    let p = 0;
+    let q = 0;
+    let rooms = 0;
+    let maxRooms = 0;
+
+    while (p < start.length) {
+        if (start[p] < end[q]) {
+            rooms++;
+            p++;
+        } else {
+            rooms--;
+            q++;
+        }
+        maxRooms = Math.max(rooms, maxRooms);
+    }
+
+    return maxRooms;
 };
 
-// Time: O(N^2), Space: O(M)
+// var minMeetingRooms = function(intervals) {
+//     if (intervals.length === 1) return 1;
+//
+//     const latestTime = intervals.reduce((max, interval) => {
+//         return Math.max(max, interval[1]);
+//     }, 0);
+//
+//     const meetingTracker = new Array(latestTime+1).fill(0);
+//
+//     for (let i = 0; i < intervals.length; i++) {
+//         const interval = intervals[i];
+//         for (let j = interval[0]; j < interval[1]; j++) {
+//             meetingTracker[j]++;
+//         }
+//     }
+//
+//     return Math.max(...meetingTracker);
+// };
+//
+// // Time: O(N^2), Space: O(M)
