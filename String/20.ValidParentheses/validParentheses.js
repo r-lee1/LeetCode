125ea470e420129280cbe73036c05b6810c4fe5b
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2021-09-05 17:27:54 -0700

    Improve ValidParentheses

--- String/20.ValidParentheses/validParentheses.js
+++ String/20.ValidParentheses/validParentheses.js
@@ -2,35 +2,63 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
 
-    if (s.length === 0) {
-        return true;
-    }
+ var isValid = function(s) {
+    const bracketMap = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    };
 
-    const openBracketStack = [];
+    const openBrackets = [];
 
-    for(let i = 0; i < s.length; i++) {
-        const char = s.charAt(i);
-        if (char === '(' || char === '{' || char === '[') {
-            openBracketStack.push(char);
-        } else if (char === ')' || char === '}' || char === ']') {
-            const matchingOpenBracket = openBracketStack.pop();
-            if (matchingOpenBracket === '(' && char === ')') {
-              continue;
-            } else if (matchingOpenBracket === '{' && char === '}') {
-              continue;
-            } else if (matchingOpenBracket === '[' && char === ']') {
-              continue;
-            } else {
+    for (let bracket of s) {
+        if (bracketMap[bracket]) {
+            openBrackets.push(bracket);
+        } else {
+            if (bracketMap[openBrackets.pop()] !== bracket) {
                 return false;
             }
         }
     }
 
-    if (openBracketStack.length > 0) {
-        return false;
-    }
-
-    return true;
+    return openBrackets.length === 0 ? true : false;
 };
+
+// Time: O(N) - go through s once
+// Space: O(N) - stack
+
+
+
+// var isValid = function(s) {
+//
+//     if (s.length === 0) {
+//         return true;
+//     }
+//
+//     const openBracketStack = [];
+//
+//     for(let i = 0; i < s.length; i++) {
+//         const char = s.charAt(i);
+//         if (char === '(' || char === '{' || char === '[') {
+//             openBracketStack.push(char);
+//         } else if (char === ')' || char === '}' || char === ']') {
+//             const matchingOpenBracket = openBracketStack.pop();
+//             if (matchingOpenBracket === '(' && char === ')') {
+//               continue;
+//             } else if (matchingOpenBracket === '{' && char === '}') {
+//               continue;
+//             } else if (matchingOpenBracket === '[' && char === ']') {
+//               continue;
+//             } else {
+//                 return false;
+//             }
+//         }
+//     }
+//
+//     if (openBracketStack.length > 0) {
+//         return false;
+//     }
+//
+//     return true;
+// };
